ba8e7232029b41dd067df5dbc401c311bbb438a9
CHO <bsuysheep927@gmail.com>
2023-02-13 21:35:36 +0800

    DE 20230214 小改版

--- js/public.js
+++ js/public.js
@@ -3,13 +3,14 @@
 
 	// navbar start
 	$('.brand-origin-show').click(function() {
-		$('#happy-skin').hide();
-		$('#special-feature').hide();
-		$('#special-offer').hide();
-		// 20221227 新年活動 隱藏哪裡買區塊 satrt
-		$('#where-buy').hide();
-		$('.navbar').removeClass('spceial_color where_color');
-		// 20221227 新年活動 隱藏哪裡買區塊 end
+		// 20230214 小改版 調整隱藏區塊寫法修改 start
+		$('#happy-skin, #special-feature, #special-offer, #where-buy, #drunk-star').hide();
+		// 20230214 小改版 調整隱藏區塊寫法修改 end
+
+		// 20230214 小改版 加入 drunk_color start 
+		$('.navbar').removeClass('spceial_color where_color drunk_color');
+		// 20230214 小改版 加入 drunk_color end
+
 		$('.navbar').addClass('origin_color');
 		$('.nav-link').removeClass('active');
 		$(this).find('.nav-link').addClass('active');
@@ -20,17 +21,17 @@
 		$('.brand-origin-slide').addClass('slide-right-2')
 		$('.brand-origin-slide-2').addClass('slide-left');	
 		$('.brand-origin-slide-3').addClass('slide-up');
-		
 	})
 
 	$('.happy-skin-show').click(function() {
-		$('#special-feature').hide();
-		$('#brand-origin').hide();
-		$('#special-offer').hide();
-		// 20221227 新年活動 隱藏哪裡買區塊 satrt
-		$('#where-buy').hide();
-		$('.navbar').removeClass('spceial_color origin_color where_color');
-		// 20221227 新年活動 隱藏哪裡買區塊 end
+		// 20230214 小改版 調整隱藏區塊寫法修改 start
+		$('#brand-origin, #special-feature, #special-offer, #where-buy, #drunk-star').hide();
+		// 20230214 小改版 調整隱藏區塊寫法修改 end
+
+		// 20230214 小改版 加入 drunk_color start
+		$('.navbar').removeClass('spceial_color origin_color where_color drunk_color');
+		// 20230214 小改版 加入 drunk_color end 
+
 		$('.nav-link').removeClass('active');
 		$(this).find('.nav-link').addClass('active');
 		$('#happy-skin').show("slide", { direction: "down" }, 10);
@@ -42,13 +43,14 @@
 	})
 
 	$('.special-feature-show').click(function() {
-		$('#happy-skin').hide();
-		$('#brand-origin').hide();
-		$('#special-offer').hide();
-		// 20221227 新年活動 隱藏哪裡買區塊 satrt
-		$('#where-buy').hide();
-		$('.navbar').removeClass('origin_color where_color');
-		// 20221227 新年活動 隱藏哪裡買區塊 end
+		// 20230214 小改版 調整隱藏區塊寫法修改 start
+		$('#brand-origin, #happy-skin, #special-offer, #where-buy, #drunk-star').hide();
+		// 20230214 小改版 調整隱藏區塊寫法修改 end
+
+		// 20230214 小改版 加入 drunk_color start 
+		$('.navbar').removeClass('origin_color where_color drunk_color');
+		// 20230214 小改版 加入 drunk_color end
+
 		$('.navbar').addClass('spceial_color');
 		$('.nav-link').removeClass('active');
 		$(this).find('.nav-link').addClass('active');
@@ -59,13 +61,14 @@
 	})
 
 	$('.special-offer-show').click(function() {
-		$('#happy-skin').hide();
-		$('#brand-origin').hide();
-		$('#special-feature').hide();
-		// 20221227 新年活動 隱藏哪裡買區塊 satrt
-		$('#where-buy').hide();
-		$('.navbar').removeClass('origin_color where_color');
-		// 20221227 新年活動 隱藏哪裡買區塊 end
+		// 20230214 小改版 調整隱藏區塊寫法修改 start
+		$('#brand-origin, #happy-skin, #special-feature, #where-buy, #drunk-star').hide();
+		// 20230214 小改版 調整隱藏區塊寫法修改 end
+
+		// 20230214 小改版 加入 drunk_color start 
+		$('.navbar').removeClass('origin_color where_color drunk_color');
+		// 20230214 小改版 加入 drunk_color end
+
 		$('.navbar').addClass('spceial_color');
 		$('.nav-link').removeClass('active');
 		$(this).find('.nav-link').addClass('active');
@@ -73,24 +76,81 @@
 		$('*').removeClass('slide-up slide-up-2 slide-right slide-right-2 slide-right-3 slide-left');
 	})
 
-	// 20221227 新年活動 新增哪裡買區塊 satrt
 	$('.where-buy-show').click(function() {
-		$('#happy-skin').hide();
-		$('#brand-origin').hide();
-		$('#special-feature').hide();
-		$('#special-offer').hide();
-		$('.navbar').removeClass('origin_color spceial_color');
+		// 20230214 小改版 調整隱藏區塊寫法修改 start
+		$('#brand-origin, #happy-skin, #special-feature, #special-offer, #drunk-star').hide();
+		// 20230214 小改版 調整隱藏區塊寫法修改 end 
+
+		// 20230214 小改版 加入 drunk_color start 
+		$('.navbar').removeClass('origin_color spceial_color drunk_color');
+		// 20230214 小改版 加入 drunk_color end
 		$('.navbar').addClass('where_color');
 		$('.nav-link').removeClass('active');
 		$(this).find('.nav-link').addClass('active');
 		$('#where-buy').show("slide", { direction: "right" }, 400);
 		$('*').removeClass('slide-up slide-up-2 slide-right slide-right-2 slide-right-3 slide-left');
 	})
-	// 20221227 新年活動 新增哪裡買區塊 end
+
+	// 20230214 小改版 醉神修護雙星區塊 start
+	$('.drunk-star-show').click(function() {
+		$('*').removeClass('slide-up slide-up-2 slide-right slide-right-2 slide-right-3 slide-left');
+		$('#brand-origin, #happy-skin, #special-feature, #special-offer, #where-buy').hide();
+		$('.navbar').removeClass('origin_color spceial_color where_color');
+		$('.navbar').addClass('drunk_color');
+		$('.nav-link').removeClass('active');
+		$(this).find('.nav-link').addClass('active');
+		$('#drunk-star').show("slide", { direction: "right" }, 400);
+	})
+	// 20230214 小改版 醉神修護雙星區塊 end
 
 	// navbar end
 
-	// slick start
+	// 20230214 小改版 banner 點擊跳至表單區塊 start
+	$('.banner').on('click', function() {
+		let top = $('.form').offset().top;
+	
+		$('.drunk-star-show').click();
+		$('html,body').animate({
+			scrollTop: top,
+		}, 700)
+	})
+	// 20230214 小改版 banner 點擊跳至表單區塊 end
+
+	// 20230214 小改版 醉神修護雙星區塊輪播 start
+	
+	$('.slider_for').slick({
+		centerMode: true,
+		centerPadding: '100px',
+		slidesToShow: 1,
+		infinite: true,
+		dots: true,
+		variableWidth: true,
+		arrows: true,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 769,
+				settings: {
+					centerPadding: '50px',
+					slidesToShow: 1,
+					variableWidth: false,
+				}
+			},
+			{
+				breakpoint: 451,
+				settings: {
+					centerPadding: '40px',
+					slidesToShow: 1,
+					variableWidth: false,
+				}
+			}
+		]
+	});	
+
+	// drunk star slick end
+	// 20230214 小改版 醉神修護雙星區塊輪播 end
+
+	// elephant slick start
 	$('.slick-items').slick({
 		infinite: true,
 		slidesToShow: 4,
@@ -107,7 +167,7 @@
 			}
 		]
 	});
-	// slick end
+	// elephant slick end
 	
 	// web btn click start
 	$('.btn_w').click(function() {
@@ -205,7 +265,6 @@
 			}
 		} else {
 			// brand-origin 
-			// console.log(scroll, $('.navbar').offset().top)		;
 			if($('#brand-origin').css('display') !== 'none') {
 				if(scroll > $('.navbar').offset().top - 460) {
 					$('.brand-origin-slide-1').addClass('slide-right');	
@@ -216,7 +275,6 @@
 			}
 			// happy-skin
 			if($('#happy-skin').css('display') != 'none') {
-				// $('.banner').offset().top && scroll > $('.banner').outerHeight() / 2
 				if(scroll > $('.navbar').offset().top - 350) {
 					$('.happy-skin-title-slide-up').addClass('slide-up');	
 					$('.happy-skin-item1-slide-up').addClass('slide-up');	
